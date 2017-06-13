fc78d4136effd443bae13f6a8933d995227a7502
Orenef11 <orenef11@gmail.com>
2017-06-13 18:03:35 +0300

    Fix functions in the convert_image file and make it generic.

--- hw2/Part A/neural_network.py
+++ hw2/Part A/neural_network.py
@@ -1,5 +1,11 @@
-from random import random
+from random import uniform
 from math import exp
+from os import path
+from pickle import dump, HIGHEST_PROTOCOL
+
+
+BOTTOM_RANGE = -0.3
+UPPER_RANGE = 0.3
 
 
 def neuron_activation(neuron_value):
@@ -10,11 +16,11 @@ def neuron_activation_derivative(activation_output):
     return activation_output * (1.0 - activation_output)
 
 
-class Neuron:
+class Neuron(object):
     def __init__(self, number_of_neurons_in_previous_layer):
         self.value = 0
         self.error = 0
-        self.weights = [(2/3.0) * random() - 1.0 / 3 for _ in range(number_of_neurons_in_previous_layer)]
+        self.weights = [uniform(BOTTOM_RANGE, UPPER_RANGE) for _ in range(number_of_neurons_in_previous_layer)]
 
     def get_value(self):
         return self.value
@@ -39,31 +45,66 @@ class Neuron:
             self.weights[index] += network_learning_rate * self.get_error() * previous_layer[index].get_value()
 
 
-class Network:
-    def __init__(self, input_size, output_size):
+class Network(object):
+    def __init__(self, input_size, number_of_neurons_in_hidden_layer, output_size, learning_rate, training_samples):
         self.output_layer = []
         self.hidden_layer = [Neuron(input_size) for _ in range(number_of_neurons_in_hidden_layer)]
         bias_neuron = Neuron(input_size)
         bias_neuron.value = 1
         self.hidden_layer.append(bias_neuron)
         self.output_layer = [Neuron(len(self.hidden_layer)) for _ in range(output_size)]
+        self.__epochs = 1
+        self.__learning_rate = learning_rate
+        self.__training_samples = training_samples
+        self.__error_rate = None
 
-    def calculate_net_output(self, input_neurons):
+    def __calculate_net_output(self, input_neurons):
         for neuron in self.hidden_layer[:-1]:
             neuron.calculate_neuron_value(input_neurons)
         for neuron in self.output_layer:
             neuron.calculate_neuron_value(self.hidden_layer)
         return [output_neuron.get_value() for output_neuron in self.output_layer]
 
-    def calculate_neurons_error(self, expected_values):
+    def __calculate_neurons_error(self, expected_values):
         next_layer = self.output_layer
         for neuron, expected_value in zip(self.output_layer, expected_values):
             neuron.error = (expected_value - neuron.get_value()) * neuron_activation_derivative(neuron.get_value())
         for neuron_index, neuron in enumerate(self.hidden_layer):
             neuron.calculate_error_by_neurons_layer(neuron_index, next_layer)
 
-    def update_neurons_weights(self, input_neurons, network_learning_rate):
+    def __update_neurons_weights(self, input_neurons, network_learning_rate):
         for neuron in self.hidden_layer:
             neuron.update_neuron_weights(input_neurons, network_learning_rate)
         for neuron in self.output_layer:
             neuron.update_neuron_weights(self.hidden_layer, network_learning_rate)
+
+    def training_neurons_network(self, input_neurons):
+        self.__epochs = 1
+        stop_network_learning = False
+        last_error_rate = 0
+        while stop_network_learning is False:
+            error_rate = 0
+            for train_input in self.__training_samples:
+                expected_output_values = train_input[:]
+                for input_neuron_index, input_field in zip(range(len(input_neurons)), train_input):
+                    input_neurons[input_neuron_index].value = input_field
+                output_values = self.__calculate_net_output(input_neurons)
+                for output_value, expected_output_value in zip(output_values, expected_output_values):
+                    error_rate += (expected_output_value - output_value) ** 2
+                self.__calculate_neurons_error(expected_output_values)
+                self.__update_neurons_weights(input_neurons, self.__learning_rate)
+            is_first_learning_iteration = self.__epochs == 1
+            if is_first_learning_iteration is False:
+                stop_network_learning = last_error_rate < error_rate
+            last_error_rate = error_rate
+            self.__epochs += 1
+        self.__error_rate = last_error_rate
+
+    def dump_nn_to_pickle(self, model_num, folder_path):
+        filename = "model_num_-{}-_hidden_size_-{}-_learning_rate_-{}"\
+            .format(str(model_num), str(len(self.hidden_layer)), str(self.__learning_rate)) + '.txt'
+        with open(path.join(folder_path, filename), 'wb') as model_file:
+            dump(self, model_file, HIGHEST_PROTOCOL)
+
+    def __str__(self):
+        print("BlaBlaBla {}".format(self.__learning_rate))
--- hw2/Part A/supporting_functions.py
+++ hw2/Part A/supporting_functions.py
@@ -16,20 +16,16 @@ def _gather_network_weights(network, input_neurons):
     return weights_collection
 
 
-def training_neurons_network(network,
-                             training_samples,
-                             input_neurons,
-                             network_learning_rate,
-                             neurons_activation_func_name,
-                             number_of_hidden_layer_neurons):
+def training_neurons_network(network, training_samples, input_neurons, network_learning_rate,
+                             neurons_activation_func_name, number_of_hidden_layer_neurons):
     epoch = 1
     stop_network_learning = False
     last_error_rate = 0
     while stop_network_learning is False:
         error_rate = 0
-        for input in training_samples:
-            expected_output_values = input[:]
-            for input_neuron_index, input_field in zip(range(len(input_neurons)), input):
+        for train_input in training_samples:
+            expected_output_values = train_input[:]
+            for input_neuron_index, input_field in zip(range(len(input_neurons)), train_input):
                 input_neurons[input_neuron_index].value = input_field
             output_values = network.calculate_net_output(input_neurons)
             for output_value, expected_output_value in zip(output_values, expected_output_values):
@@ -45,7 +41,7 @@ def training_neurons_network(network,
         trained_network_weights_file_name += "_learning_rate_" + str(network_learning_rate)
         trained_network_weights_file_name += "_activation_func_" + neurons_activation_func_name + ".pickle"
         with open(trained_network_weights_file_name, 'wb') as results_file:
-            dump(_gather_network_weights(network), results_file, protocol=HIGHEST_PROTOCOL)
+            dump(network, results_file, protocol=HIGHEST_PROTOCOL)
 
 
 def normailze_sample_data(data_row):
@@ -57,6 +53,12 @@ def normailze_sample_data(data_row):
                 data_row_values_max_difference = float(max_value - min_value)
                 normalized_data_row.append((data_row[field_index] - min_value) / data_row_values_max_difference)
             else:
-                data_norm = sqrt(sum(map(lambda x: x ** 2, data_row)))
+                data_norm = (sum(map(lambda x: x ** 2, data_row))) ** 0.5
                 normalized_data_row.append(data_row[field_index] / data_norm)
         return normalized_data_row
+
+
+def normalize_image_data(image, min_pixel_value, max_pixel_value):
+    diff_min_and_max = max_pixel_value - min_pixel_value
+    image = [(pixel - min_pixel_value) / diff_min_and_max for pixel in [row for row in image]]
+    return image
