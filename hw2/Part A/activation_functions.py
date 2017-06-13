707a70cabd2e74a8592ecdf48eec0a64910cab0f
Tal-D <tdan.m92@gmail.com>
2017-06-13 23:05:52 +0300

    small fix

--- hw2/Part A/activation_functions.py
+++ hw2/Part A/activation_functions.py
@@ -5,7 +5,7 @@ def sigmoid_activation_function(neuron_value):
     return 1.0 / (1.0 + exp(-neuron_value))
 
 
-def sigmoid_activation_function_derivative(activation_output)
+def sigmoid_activation_function_derivative(activation_output):
     return activation_output * (1.0 - activation_output)
 
 
@@ -13,7 +13,7 @@ def bipolar_sigmoid_activation_function(neuron_value):
     return (1.0 - exp(-neuron_value)) / (1.0 + exp(-neuron_value))
 
 
-def bipolar_sigmoid_activation_function_derivative(activation_output)
+def bipolar_sigmoid_activation_function_derivative(activation_output):
     return 0.5 * (1.0 + activation_output) * (1.0 - activation_output)
 
 
@@ -21,5 +21,5 @@ def hyperbolic_tangent_activation_function(neuron_value):
     return (1.0 - exp(-2 * neuron_value)) / (1.0 + exp(-2 * neuron_value))
 
 
-def hyperbolic_tangent_activation_function_derivative(activation_output)
+def hyperbolic_tangent_activation_function_derivative(activation_output):
     return (1.0 + activation_output) * (1.0 - activation_output)
