4d07ff370e7226492886c3f565cae1dd20758c40
Tal-D <tdan.m92@gmail.com>
2017-06-21 03:51:21 +0300

    moved each functions couple to interface class

--- hw2/Part A/activation_functions.py
+++ hw2/Part A/activation_functions.py
@@ -1,34 +1,48 @@
 from math import exp
 
 
-def sigmoid_activation_function(neuron_value):
-    try:
-        return 1.0 / (1.0 + exp(-neuron_value))
-    except:
-        return -1
+class Base_Activation_Function_Interface:
+    def display_function_type_name():
+        print "The function type is " + self.type_name
 
 
-def sigmoid_activation_function_derivative(activation_output):
-    return activation_output * (1.0 - activation_output)
+class Sigmoid_Activation_Function_Interface(Base_Activation_Function_Interface):
+    def __init__(self):
+        self.type_name = "sigmoid"
+
+    def sigmoid_activation_function(self, neuron_value):
+        try:
+            return 1.0 / (1.0 + exp(-neuron_value))
+        except:
+            return -1
+
+    def sigmoid_activation_function_derivative(self, activation_output):
+        return activation_output * (1.0 - activation_output)
 
 
-def bipolar_sigmoid_activation_function(neuron_value):
-    try:
-        return (1.0 - exp(-neuron_value)) / (1.0 + exp(-neuron_value))
-    except:
-        return -1
+class Bipolar_Sigmoid_Activation_Function_Interface(Base_Activation_Function_Interface):
+    def __init__(self):
+        self.type_name = "bipolar sigmoid"
+
+    def bipolar_sigmoid_activation_function(neuron_value):
+        try:
+            return (1.0 - exp(-neuron_value)) / (1.0 + exp(-neuron_value))
+        except:
+            return -1
+
+    def bipolar_sigmoid_activation_function_derivative(activation_output):
+        return 0.5 * (1.0 + activation_output) * (1.0 - activation_output)
 
 
-def bipolar_sigmoid_activation_function_derivative(activation_output):
-    return 0.5 * (1.0 + activation_output) * (1.0 - activation_output)
+class Hyperbolic_Tangent_Activation_Function_Interface(Base_Activation_Function_Interface):
+    def __init__(self):
+        self.type_name = "hyperbolic tangent"
 
+    def hyperbolic_tangent_activation_function(neuron_value):
+        try:
+            return (1.0 - exp(-2 * neuron_value)) / (1.0 + exp(-2 * neuron_value))
+        except:
+            return -1
 
-def hyperbolic_tangent_activation_function(neuron_value):
-    try:
-        return (1.0 - exp(-2 * neuron_value)) / (1.0 + exp(-2 * neuron_value))
-    except:
-        return -1
-
-
-def hyperbolic_tangent_activation_function_derivative(activation_output):
-    return (1.0 + activation_output) * (1.0 - activation_output)
+    def hyperbolic_tangent_activation_function_derivative(activation_output):
+        return (1.0 + activation_output) * (1.0 - activation_output)
