f2e9274f72429b6032bbec3991ba2e98cff0d6b9
Tal-D <tdan.m92@gmail.com>
2017-06-25 20:22:02 +0300

    added neuron neighbours field for Neuron() objects
    By adding the new fields and updating the relevant KohonenNetwork, Neuron we can calculate every neurons neighbour when we initialize the network. After that we are able to use the neurons new neighbours fields (instead of using a function that find the neighbours).

--- hw2/part B/kohonen_network.py
+++ hw2/part B/kohonen_network.py
@@ -8,11 +8,18 @@ NUMBER_OF_PLOT_DIMENSIONS = 2
 class Neuron(object):
     def __init__(self):
         self._neuron_location_parameters = []
+        self._neighbours = []
         self._initialize_neuron_location()
 
     def _initialize_neuron_location(self):
         for _ in range(NUMBER_OF_PLOT_DIMENSIONS):
             self._neuron_location_parameters.append(random() * 0.05 + 0.4)  # temporary values choice!
+
+    def set_neuron_neighbours(self, neighbours):
+        self._neighbours = neighbours
+
+    def get_neuron_neighbours(self):
+        return self._neighbours
 
     def draw_neuron(self):
         plot(self._neuron_location_parameters[0], self._neuron_location_parameters[1], "ro", color="blue")
@@ -27,6 +34,20 @@ class KohonenNetwork(object):
         self._number_of_neurons = self._calculate_number_of_network_neurons()
         self._network_neurons = {}
         self._initialize_network_neurons()
+        for (neuron_indexes, neuron) in self._network_neurons.items():
+            neighbours = self._find_neuron_neighbours(neuron_indexes)
+            neuron.set_neuron_neighbours(neighbours)
+
+    def _find_neuron_neighbours(self, neuron_indexes):
+        neighbours_collection = []
+        number_of_indexes = len(neuron_indexes)
+        for increased_index_place in range(number_of_indexes):
+            searched_indexes = list(neuron_indexes[:])
+            searched_indexes[increased_index_place] += 1
+            search_result = self._network_neurons.get(tuple(searched_indexes), None)
+            if search_result is not None:
+                neighbours_collection.append(search_result)
+        return neighbours_collection
 
     def _initialize_network_neurons(self):
         neurons_indexes_by_network_dimensions = []
@@ -45,21 +66,9 @@ class KohonenNetwork(object):
         for neuron in self._network_neurons.values():
             neuron.draw_neuron()
 
-    def _find_neuron_neighbours(self, neuron_indexes):
-        neighbours_collection = []
-        number_of_indexes = len(neuron_indexes)
-        for increased_index_place in range(number_of_indexes):
-            searched_indexes = list(neuron_indexes[:])
-            searched_indexes[increased_index_place] += 1
-            search_result = self._network_neurons.get(tuple(searched_indexes), None)
-            if search_result is not None:
-                neighbours_collection.append(search_result)
-        return neighbours_collection
-
-    def _draw_neurons_connection_lines(self):
-        for (neuron_indexes, neuron) in self._network_neurons.items():
-            neuron_neighbours = self._find_neuron_neighbours(neuron_indexes)
-            for neuron_neighbour in neuron_neighbours:
+    def _draw_neurons_connections(self):
+        for neuron in self._network_neurons.values():
+            for neuron_neighbour in neuron.get_neuron_neighbours():
                 neuron_location = neuron.get_neuron_location()
                 neuron_neighbour_location = neuron_neighbour.get_neuron_location()
                 plot([neuron_location[0], neuron_neighbour_location[0]],
@@ -68,4 +77,4 @@ class KohonenNetwork(object):
 
     def draw_network(self):
         self._draw_neurons()
-        self._draw_neurons_connection_lines()
+        self._draw_neurons_connections()
